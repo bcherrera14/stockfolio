cda662454295974ff3a19ebd679bfee1a04a9c9b
Bryan Herrera <bcherrera14@gmail.com>
2022-01-03 15:22:52 -0800

    add nhibernate

---
+++ API/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class User
+    {
+        public virtual string id { get; set; }
+        public virtual string firstname { get; set; }
+        public virtual string lastname { get; set; }
+        public virtual string username { get; set; }
+        public virtual string password { get; set; }
+        public virtual decimal accountbalance { get; set; }
+    }
+}
