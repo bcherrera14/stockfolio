fd84d93a857f6d165ab49841b5b0bcc20971e20b
Bryan Herrera <bcherrera14@gmail.com>
2022-01-04 07:39:47 -0800

    navigate to portfolio on login

--- API/client/src/Components/App.js
+++ API/client/src/Components/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink, useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 import Landing from './Landing';
 import Portfolio from './Portfolio';
-import history from '../History';
 
 const App = () => {
+	let navigate = useNavigate();
 	return (
 		<div>
-			<Routes history={history}>
+			<Routes >
 				<Route path="/" element={<Navbar />}>
-					<Route index element={<Landing />} />
+					<Route index element={<Landing navigate={navigate} />} />
 					<Route path="/portfolio" element={<Portfolio />} />
 				</Route>
 			</Routes>
